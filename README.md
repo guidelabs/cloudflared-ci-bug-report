920fde661b1ded82ac1f782f9a87910e3ac467db
Andreas Madsen <amwebdk@gmail.com>
2026-01-12 11:20:54 -0800

    reproduce

---
+++ README.md
@@ -0,0 +1,5 @@
+# To reproduce
+
+1. Replace `REPALCE_ME` in wrangler.jsonc. This is to specify account_id, worker name, and db name.
+2. npm install
+3. npm run test
---
+++ test/example.test.js
@@ -0,0 +1,7 @@
+import { expect, test, describe } from "vitest";
+
+describe("A", async() => {
+    test("B", async() => {
+        expect('A').toEqual('A');
+    })
+})
