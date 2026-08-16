1b930387c8e567ec3d412f5b6fb68480c562d792
Andreas Madsen <amwebdk@gmail.com>
2026-08-17 08:16:30 +0900

    update to non-deprecated model

--- test/one.test.js
+++ test/one.test.js
@@ -3,7 +3,7 @@ import { env } from "cloudflare:workers";
 
 describe("A", async() => {
     test("B", async() => {
-        const response = await env.AI.run("@cf/meta/llama-3-8b-instruct", { prompt: "Hi! Who are you?" });
+        const response = await env.AI.run("@cf/meta/llama-3.2-1b-instruct", { prompt: "Hi! Who are you?" });
         expect(response).not.toBeUndefined();
     })
 }, 30000)
